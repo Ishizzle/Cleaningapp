101173d03fe16c9bd5d6a358cb50b49d73148d09
Ishizzle <sangha.inder@gmail.com>
2025-03-03 19:25:51 -0700

    Delete models/routes directory

--- models/routes/routes/routes/middleware/authMiddleware.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const authMiddleware = (req, res, next) => {
-  // JWT authentication logic
-};
-
-module.exports = authMiddleware;
--- models/routes/routes/routes/reportRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Report = require('../models/Report');
-
-router.post('/', async (req, res) => {
-  // Report an incident
-});
-
-module.exports = router;
