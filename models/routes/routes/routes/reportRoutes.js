7bf97f3843697e4948aa38663170339aa4793671
Ishizzle <sangha.inder@gmail.com>
2025-03-03 19:03:14 -0700

    Create reportRoutes.js

---
+++ models/routes/routes/routes/reportRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const Report = require('../models/Report');
+
+router.post('/', async (req, res) => {
+  // Report an incident
+});
+
+module.exports = router;
