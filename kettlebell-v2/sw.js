8a5123fde467c89e423dd7f4d566e5d81737e636
Ishizzle <sangha.inder@gmail.com>
2026-07-11 14:13:14 -0600

    Add offline caching for upgraded app

---
+++ kettlebell-v2/sw.js
@@ -0,0 +1,5 @@
+const CACHE='kb-coach-v2-1';
+const ASSETS=['./','./index.html','./styles.css','./program.js','./app.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request))));
