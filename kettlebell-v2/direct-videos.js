7c1ed8fd0f5105db7e9e66c92ac742f5bc7212cb
Ishizzle <sangha.inder@gmail.com>
2026-07-11 16:05:51 -0600

    Open direct exercise tutorial videos

---
+++ kettlebell-v2/direct-videos.js
@@ -0,0 +1,80 @@
+// Direct exercise tutorial links. These replace YouTube search-result links so
+// tapping a video button opens the selected video immediately.
+Object.assign(CURATED, {
+  'Goblet Squat': {
+    coach: 'Scott Herman',
+    url: 'https://www.youtube.com/watch?v=MeIiIdhvXT4'
+  },
+  'Kettlebell Swing': {
+    coach: 'Mark Wildman',
+    url: 'https://www.youtube.com/watch?v=m-S9H2XVvYg'
+  },
+  'Single Arm Overhead Press': {
+    coach: 'Mark Wildman',
+    url: 'https://www.youtube.com/watch?v=XHkI03S6Pls'
+  },
+  'Single Arm Floor Press': {
+    coach: 'Mark Wildman',
+    url: 'https://www.youtube.com/watch?v=KTDVl0Lkdc4&list=PLk4oYPJ7TXKhX8YqA2AVrfgs_pEF6p7KA&index=28'
+  },
+  'Half Kneeling Press': {
+    coach: 'Mark Wildman',
+    url: 'https://www.youtube.com/watch?v=KTDVl0Lkdc4&list=PLk4oYPJ7TXKhX8YqA2AVrfgs_pEF6p7KA&index=29'
+  },
+  'Push Press': {
+    coach: 'Mark Wildman',
+    url: 'https://www.youtube.com/watch?v=KTDVl0Lkdc4&list=PLk4oYPJ7TXKhX8YqA2AVrfgs_pEF6p7KA&index=49'
+  },
+  'Single Arm Row': {
+    coach: 'Scott Herman',
+    url: 'https://www.youtube.com/watch?v=pYcpY20QaE8'
+  },
+  'Single Arm Clean': {
+    coach: 'Mark Wildman',
+    url: 'https://www.youtube.com/watch?v=_nIsSXWs5iU'
+  },
+  'Turkish Get-up': {
+    coach: 'Exercise tutorial',
+    url: 'https://www.youtube.com/watch?v=saYKvqSscuY'
+  },
+  'Bulgarian Split Squat': {
+    coach: 'Scott Herman',
+    url: 'https://www.youtube.com/watch?v=2C-uNgKwPLE'
+  },
+  'Single Leg Romanian Deadlift': {
+    coach: 'Scott Herman',
+    url: 'https://www.youtube.com/watch?v=7g9s6fK4gO0'
+  },
+  'Reverse Lunge': {
+    coach: 'Exercise tutorial',
+    url: 'https://www.youtube.com/watch?v=xrPteyQLGAo'
+  },
+  'Suitcase Carry': {
+    coach: 'Exercise tutorial',
+    url: 'https://www.youtube.com/watch?v=y-hn_Ha1-RE'
+  },
+  'Push-up': {
+    coach: 'Calisthenicmovement',
+    url: 'https://www.youtube.com/watch?v=IODxDxX7oi4'
+  },
+  'Plank': {
+    coach: 'Exercise tutorial',
+    url: 'https://www.youtube.com/watch?v=pvIjsG5Svck'
+  },
+  'Side Plank': {
+    coach: 'Exercise tutorial',
+    url: 'https://www.youtube.com/watch?v=N_s9em1xTqU'
+  },
+  'Dead Bug': {
+    coach: 'Squat University',
+    url: 'https://www.youtube.com/watch?v=I5xbsA71v1A'
+  },
+  'Mountain Climber': {
+    coach: 'Exercise tutorial',
+    url: 'https://www.youtube.com/watch?v=kLh-uczlPLg'
+  },
+  'Hammer Curl': {
+    coach: 'Scott Herman',
+    url: 'https://www.youtube.com/watch?v=XOEL4MgekYE'
+  }
+});
